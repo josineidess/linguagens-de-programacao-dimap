f6f9a56ad6f0d639fb64a7073de3a891d3f092f5
Josineide Soares <josineide1402@gmail.com>
2024-11-26 08:23:26 -0300

    trabalhando com arranjos

--- aula17/main.c
+++ aula17/main.c
@@ -9,7 +9,15 @@ int main() {
      //compilar todo mundo: gcc -o programa main.o func1.o func2.o
      //executa programa: ./programa
      //remove os arquivos o: rm *.o
-    printf("2*%d = %d;", num, dobr(num));
-    printf("3*%d = %d;", num, tripl(num));
+    //printf("2*%d = %d;", num, dobr(num));
+    //printf("3*%d = %d;", num, tripl(num));
+
+    int arr[5] = {1,2,3,4,5};
+    int arr2[5] = {}; //todos os elementos são 0
+    *(arr + 1) = 8;
+    printf("arr1 = %d;",arr[0]);
+    printf("arr2 = %d;", arr2[0]);
+    printf("arr1 = %d;", *(arr + 1));
+    
     return 0;
 }
